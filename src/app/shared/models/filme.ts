01adc2d8f25035acd4db7d7242369cd52f3523e0
Iago Duarte de Aguiar <iagoaguiar202@gmail.com>
2021-08-31 11:00:39 -0300

    Formulario de cadastro de filmes
    Feito o formulario de cadastro de filmes, com componentes reutilizaveis
    para cada campo, podendo ser usados em outros fomularios se necessarios

---
+++ src/app/shared/models/filme.ts
@@ -0,0 +1,12 @@
+export interface Filme {
+
+
+    id?: number;
+    titulo: string;
+    urlFoto?: string;
+    dtLancamento: Date;
+    descricao?: string;
+    urlIMDB?:string;
+    genero: string;
+
+}
